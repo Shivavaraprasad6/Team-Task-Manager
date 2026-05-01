9d34be8703143cdef126905eac0354131ad4218d
Shivavaraprasad6 <shivavaraprasadkaithoju93@gmail.com>
2026-05-01 16:10:14 +0530

    Clean up config.php by removing old connection code
    Removed commented-out old database connection code.

--- config.php
+++ config.php
@@ -1,11 +1,3 @@
-<!-- // <?php
-// $conn = new mysqli("sql305.infinityfree.com", "if0_41795929", "Shiva1122", "if0_41795929_taskm");
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-// session_start();
-// ?> -->
 
 <?php
 // Database connection using Railway Environment Variables
