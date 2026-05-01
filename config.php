2d608b1e2371bf37d1ba89e808df48380692cea9
Shivavaraprasad6 <shivavaraprasadkaithoju93@gmail.com>
2026-05-01 16:09:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,27 @@
-<?php
-$conn = new mysqli("sql305.infinityfree.com", "if0_41795929", "Shiva1122", "if0_41795929_taskm");
+<!-- // <?php
+// $conn = new mysqli("sql305.infinityfree.com", "if0_41795929", "Shiva1122", "if0_41795929_taskm");
 
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// session_start();
+// ?> -->
+
+<?php
+// Database connection using Railway Environment Variables
+$conn = new mysqli(
+    getenv('mysql.railway.internal'), 
+    getenv('root'), 
+    getenv('odWpILyaYBuvfYWZOVPVxZDlcZDMRNXV'), 
+    getenv('railway'), 
+    getenv('3306')
+);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// Start session for user login tracking
 session_start();
 ?>
